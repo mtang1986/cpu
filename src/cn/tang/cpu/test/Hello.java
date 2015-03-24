f85b856da0ccd10532101391acec5d909d107163
mtang1986 <locke@192.168.76.197>
2015-03-24 16:01:45 +0800

    Upper CPU

--- src/cn/tang/cpu/test/Hello.java
+++ src/cn/tang/cpu/test/Hello.java
@@ -2,6 +2,6 @@ package cn.tang.cpu.test;
 
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello cpu");
+        System.out.println("Hello CPU");
     }
 }
